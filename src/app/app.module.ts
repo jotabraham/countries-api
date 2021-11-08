c5c9cc84d3f915dcc3c2f4e748e2dd9febfa8111
jotabraham <jotabraham@gmail.com>
2021-11-08 14:15:55 -0500

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
+import { CountryCardComponent } from './country-card/country-card.component';
+import { CountryDetailsComponent } from './country-details/country-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    HomeComponent,
+    CountryCardComponent,
+    CountryDetailsComponent
   ],
   imports: [
     BrowserModule,
